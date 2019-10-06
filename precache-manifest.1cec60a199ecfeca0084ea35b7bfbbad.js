cc5523c8fd7e7e8c2f693cd6bc20a1e7c015556c
Alfi Azizi <azizi.alfi15@gmail.com>
2019-10-06 19:25:29 +0700

    Updates

---
+++ precache-manifest.1cec60a199ecfeca0084ea35b7bfbbad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e49517fefcb647647202726864dbde4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7c2e7fc3a7c0f956c3aa",
+    "url": "/monsters-rolodex/static/css/main.5a46b536.chunk.css"
+  },
+  {
+    "revision": "a7ffd671cb21d8ff7fb4",
+    "url": "/monsters-rolodex/static/js/2.fe5198a4.chunk.js"
+  },
+  {
+    "revision": "7c2e7fc3a7c0f956c3aa",
+    "url": "/monsters-rolodex/static/js/main.8cf8f4d6.chunk.js"
+  },
+  {
+    "revision": "d78c7e54dca26e3c25e2",
+    "url": "/monsters-rolodex/static/js/runtime-main.aaf9d0bd.js"
+  }
+]);
